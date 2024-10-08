0f188fa23bb164aa6758fd44b282e56e52fd7a7c
Geolm <moyaguillaume@gmail.com>
2024-10-07 20:17:52 -0400

    fixed macos compilation

--- tests/run_unit_tests.sh
+++ tests/run_unit_tests.sh
@@ -5,5 +5,5 @@ set -e
 clang -Wall unit_tests.c -o unit_tests
 ./unit_tests
 
-clang++ -Wall tests_against_stl.cpp -o tests_against_stl
+clang++ -Wall tests_against_stl.cpp -o tests_against_stl -std=c++11
 ./tests_against_stl
